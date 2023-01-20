04b74922f017a352b3ebb04c64cc00af0dfe28fa
albertsparrman <albert.sparrman@elev.ga.ntig.se>
2023-01-20 11:12:17 +0100

    jag commitar och pushar

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const indexRouter = require('./routes/index');
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+});
+const promisePool = pool.promise();
 
-router.get('/', async function (req, res) {
-    res.send('Hello you!')
+router.get('/', async function (req, res, next) {
+    const [rows] = await promisePool.query("SELECT * FROM as30forum");
+   
+    res.render('index.njk', {
+        rows: rows,
+        title: 'Forum',
+    });
+});
+
+router.post('/new', async function (req, res, next) {
+    const { author, title, content } = req.body;
+    const [rows] = await promisePool.query("INSERT INTO as30forum (author, title, content) VALUES (?, ?, ?)", [author, title, content]);
+    res.redirect('/');
+});
+
+router.get('/new', async function (req, res, next) {
+    res.render('new.njk', {
+        title: 'Nytt inlägg',
+    });
 });
 
 module.exports = router;
