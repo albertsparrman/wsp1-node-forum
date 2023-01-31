4ad7cc6b312311b797b6c240e327180e5214cedb
albertsparrman <albert.sparrman@elev.ga.ntig.se>
2023-01-31 15:11:18 +0100

    🍻 Jag var inte full

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const port = 3000;
 const bodyParser = require('body-parser');
 
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 nunjucks.configure('views', {
@@ -22,7 +22,7 @@ nunjucks.configure('views', {
 });
 
 app.use('/', indexRouter);
-app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+
+module.exports = router;
+
 const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -10,7 +13,7 @@ const pool = mysql.createPool({
 const promisePool = pool.promise();
 
 router.get('/', async function (req, res, next) {
-    const [rows] = await promisePool.query("SELECT * FROM as30forum");
+    const [rows] = await promisePool.query("SELECT as30forum.*, as30users.name FROM as30forum JOIN as30users ON as30forum.authorId = as30users.id");
    
     res.render('index.njk', {
         rows: rows,
@@ -18,16 +21,30 @@ router.get('/', async function (req, res, next) {
     });
 });
 
-router.post('/new', async function (req, res, next) {
-    const { author, title, content } = req.body;
-    const [rows] = await promisePool.query("INSERT INTO as30forum (author, title, content) VALUES (?, ?, ?)", [author, title, content]);
-    res.redirect('/');
-});
-
 router.get('/new', async function (req, res, next) {
+    const [users] = await promisePool.query('SELECT * FROM as30users');
     res.render('new.njk', {
         title: 'Nytt inlägg',
+        users,
     });
 });
 
-module.exports = router;
+
+router.post('/new', async function (req, res, next) {
+    const { author, title, content } = req.body;
+
+    // Skapa en ny författare om den inte finns men du behöver kontrollera om användare finns!
+    let [user] = await promisePool.query('SELECT * FROM as30users WHERE id = ?', [author]);
+    if (!user) {
+        user = await promisePool.query('INSERT INTO as30users (name) VALUES (?)', [author]);
+    }
+
+    // user.insertId bör innehålla det nya ID:t för författaren
+
+    const userId = user.insertId || user[0].id;
+
+    // kör frågan för att skapa ett nytt inlägg
+    const [rows] = await promisePool.query('INSERT INTO as30forum (authorId, title, content) VALUES (?, ?, ?)', [userId, title, content]);
+    res.redirect('/'); // den här raden kan vara bra att kommentera ut för felsökning, du kan då använda tex. res.json({rows}) för att se vad som skickas tillbaka från databasen
+});
+
